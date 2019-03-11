79cce66c3a09f8175cbdd0becfed523e4a35f61c
Abraham Silva <abraham@tenatek.com>
2019-03-11 13:32:12 -0600

    navigation drawer progress

--- app/src/main/java/mx/itesm/edu/life/Home.java
+++ app/src/main/java/mx/itesm/edu/life/Home.java
@@ -2,6 +2,7 @@ package mx.itesm.edu.life;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
