9f966dc05687a9c5da87dbad24d1c64c46ad018d
Abraham Silva <abraham@tenatek.com>
2019-03-12 02:46:59 -0600

    improved expandable list style

--- app/src/main/java/mx/itesm/edu/life/ExpandableListAdapter.java
+++ app/src/main/java/mx/itesm/edu/life/ExpandableListAdapter.java
@@ -67,7 +67,6 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
             convertView = inflater.inflate(R.layout.list_group,null);
         }
         TextView lblListHeader = (TextView)convertView.findViewById(R.id.lblListHeader);
-        lblListHeader.setTypeface(null,Typeface.BOLD);
         lblListHeader.setText(headerTitle);
         return convertView;
     }
--- app/src/main/java/mx/itesm/edu/life/FaqsFragment.java
+++ app/src/main/java/mx/itesm/edu/life/FaqsFragment.java
@@ -41,15 +41,17 @@ public class FaqsFragment extends Fragment {
         listDataHeader = new ArrayList<>();
         listHashMap = new HashMap<>();
 
-        listDataHeader.add("FAQ1");
-        listDataHeader.add("FAQ2");
-        listDataHeader.add("FAQ3");
-        listDataHeader.add("FAQ4");
-        listDataHeader.add("FAQ5");
+        listDataHeader.add("Pregunta 1");
+        listDataHeader.add("Pregunta 2");
+        listDataHeader.add("Pregunta 3");
+        listDataHeader.add("Pregunta 4");
+        listDataHeader.add("Pregunta 5");
 
         for(int i=0;i<listDataHeader.size();i++){
             listHashMap.put(listDataHeader.get(i),new ArrayList<String>());
-            listHashMap.get(listDataHeader.get(i)).add("respuesta"+i);
+            listHashMap.get(listDataHeader.get(i)).add("Lorem ipsum dolor sit amet, purus pharetra tristique quis " +
+                    "dolor sit tellus, scelerisque bibendum platea, a sed. Aliquet ornare phasellus, suspendisse odio, " +
+                    "aenean sem felis eros erat, ut etiam, ac maecenas facilisis.");
         }
 
     }
