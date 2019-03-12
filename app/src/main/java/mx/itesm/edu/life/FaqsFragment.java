e1baf5ada2a5da71789a154fa9ecd225340eb19e
Abraham Silva <abraham@tenatek.com>
2019-03-12 01:35:30 -0600

    added expandable list for FAQs

--- app/src/main/java/mx/itesm/edu/life/FaqsFragment.java
+++ app/src/main/java/mx/itesm/edu/life/FaqsFragment.java
@@ -7,11 +7,50 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static java.sql.DriverManager.println;
 
 public class FaqsFragment extends Fragment {
+
+    private ExpandableListView expandableListView;
+    private ExpandableListAdapter expandableListAdapter;
+    private List<String> listDataHeader;
+    private HashMap<String,List<String>>listHashMap;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_faqs,container,false);
+
+        View rootView = inflater.inflate(R.layout.fragment_faqs,container,false);
+
+        expandableListView = (ExpandableListView)rootView.findViewById(R.id.expListView);
+        initData();
+        expandableListAdapter = new ExpandableListAdapter(getActivity(),listDataHeader,listHashMap);
+        expandableListView.setAdapter(expandableListAdapter);
+
+        return rootView;
+    }
+
+    public void initData(){
+
+        listDataHeader = new ArrayList<>();
+        listHashMap = new HashMap<>();
+
+        listDataHeader.add("FAQ1");
+        listDataHeader.add("FAQ2");
+        listDataHeader.add("FAQ3");
+        listDataHeader.add("FAQ4");
+        listDataHeader.add("FAQ5");
+
+        for(int i=0;i<listDataHeader.size();i++){
+            listHashMap.put(listDataHeader.get(i),new ArrayList<String>());
+            listHashMap.get(listDataHeader.get(i)).add("respuesta"+i);
+        }
+
     }
 }
