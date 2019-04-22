b3c8e038e8eb90d0da071433166807e56476dac8
Abraham Silva <abraham@tenatek.com>
2019-04-21 21:46:18 -0500

    splash screen animation

---
+++ app/src/main/java/mx/itesm/edu/life/SplashActivity.java
@@ -0,0 +1,31 @@
+package mx.itesm.edu.life;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        videoView = (VideoView)findViewById(R.id.videoView);
+        Uri video = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.loop1);
+        videoView.setVideoURI(video);
+        new Handler().postDelayed(new Runnable() {// a thread in Android
+            @Override
+            public void run() {
+                Intent intent = new Intent( SplashActivity.this  ,  MainActivity.class  );
+                startActivity(intent);
+                finish();
+            }
+        },7000);
+        videoView.start();
+    }
+}
