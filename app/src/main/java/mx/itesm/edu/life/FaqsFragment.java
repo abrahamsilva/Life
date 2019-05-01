965008a649862f2a17ce41992ef01f698074c0b6
Abraham Silva <abraham@tenatek.com>
2019-05-01 17:22:52 -0500

    faqs downloaded from firebase

--- app/src/main/java/mx/itesm/edu/life/FaqsFragment.java
+++ app/src/main/java/mx/itesm/edu/life/FaqsFragment.java
@@ -1,6 +1,7 @@
 package mx.itesm.edu.life;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,11 +10,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import mx.itesm.edu.life.adapters.ExpandableListAdapter;
+import mx.itesm.edu.life.models.Contact;
+import mx.itesm.edu.life.models.Faq;
 
 public class FaqsFragment extends Fragment {
 
@@ -21,6 +30,8 @@ public class FaqsFragment extends Fragment {
     private ExpandableListAdapter expandableListAdapter;
     private List<String> listDataHeader;
     private HashMap<String,List<String>>listHashMap;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
 
     @Nullable
     @Override
@@ -28,11 +39,12 @@ public class FaqsFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_faqs,container,false);
         getActivity().setTitle(R.string.nav_faqs);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference("faqs");
+
         expandableListView = (ExpandableListView)rootView.findViewById(R.id.expListView);
         initData();
-        expandableListAdapter = new ExpandableListAdapter(getActivity(),listDataHeader,listHashMap);
-        expandableListView.setAdapter(expandableListAdapter);
-
         return rootView;
     }
 
@@ -41,6 +53,27 @@ public class FaqsFragment extends Fragment {
         listDataHeader = new ArrayList<>();
         listHashMap = new HashMap<>();
 
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot faqSnapshot : dataSnapshot.getChildren()){
+                    Faq faq = faqSnapshot.getValue(Faq.class);
+                    System.out.println(faq.getQuestion());
+                    listDataHeader.add(faq.getQuestion());
+                    listHashMap.put(listDataHeader.get(Integer.parseInt(faq.getId())),new ArrayList<String>());
+                    listHashMap.get(listDataHeader.get(Integer.parseInt(faq.getId()))).add(faq.getAnswer());
+                }
+                expandableListAdapter = new ExpandableListAdapter(getActivity(),listDataHeader,listHashMap);
+                expandableListView.setAdapter(expandableListAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+/*
         listDataHeader.add("Pregunta 1");
         listDataHeader.add("Pregunta 2");
         listDataHeader.add("Pregunta 3");
@@ -53,6 +86,6 @@ public class FaqsFragment extends Fragment {
                     "dolor sit tellus, scelerisque bibendum platea, a sed. Aliquet ornare phasellus, suspendisse odio, " +
                     "aenean sem felis eros erat, ut etiam, ac maecenas facilisis.");
         }
-
+*/
     }
 }
---
+++ app/src/main/java/mx/itesm/edu/life/models/Faq.java
@@ -0,0 +1,39 @@
+package mx.itesm.edu.life.models;
+
+public class Faq {
+
+    private String id, question, answer;
+
+    public Faq(){
+
+    }
+    public Faq(String id, String question, String answer) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
