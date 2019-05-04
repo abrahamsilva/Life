c258e6376aafde16945276aa74ae0169040d9ebb
Yoali <sb.yoali@gmail.com>
2019-05-04 11:42:27 -0500

    added animations and custom title to TipsDetailsFragment

--- app/src/main/java/mx/itesm/edu/life/TipDetailsDialog.java
+++ app/src/main/java/mx/itesm/edu/life/TipDetailsDialog.java
@@ -11,20 +11,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class TipDetailsDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View titleView = getActivity().getLayoutInflater().inflate(R.layout.dialog_title, null);
+        TextView tv = titleView.findViewById(R.id.titleBar);
+
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder( getActivity(), R.style.DialogTheme );
         Bundle mArgs = getArguments();
         String mTitle = mArgs.getString("title");
+        tv.setText(mTitle);
         String mDesc = mArgs.getString("desc");
         builder.setMessage(mDesc)
-                .setTitle(mTitle)
+                .setCustomTitle(titleView)
                 .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                     }
                 });
         return builder.create();
+    }
+
+    @Override
+    public void onStart() {
+        if (getDialog() == null)
+        {
+            return;
+        }
+
+        getDialog().getWindow().setWindowAnimations(
+                R.style.MyAnimation_Window);
+
+        super.onStart();
     }
 }
